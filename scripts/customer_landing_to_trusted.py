ec005311da38c646bb67a8cec33e3f9fbf330178
Rusul Al-Saedi <your-actual-github-email@example.com>
2026-07-26 19:44:10 -0400

    Replace script file with actual Glue job code

--- scripts/customer_landing_to_trusted.py
+++ scripts/customer_landing_to_trusted.py
@@ -1,3 +1,45 @@
-cd ~/Documents/stedi-human-balance-analytics/scripts
-pbpaste > customer_landing_to_trusted.py
-head -20 customer_landing_to_trusted.py
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
+from awsglue import DynamicFrame
+
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
+
+# Script generated for node Customer Landing
+CustomerLanding_node1785107786276 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_landing", transformation_ctx="CustomerLanding_node1785107786276")
+
+# Script generated for node Privacy Filter
+SqlQuery1489 = '''
+SELECT * FROM myDataSource
+WHERE sharewithresearchasofdate IS NOT NULL
+'''
+PrivacyFilter_node1785108086162 = sparkSqlQuery(glueContext, query = SqlQuery1489, mapping = {"myDataSource":CustomerLanding_node1785107786276}, transformation_ctx = "PrivacyFilter_node1785108086162")
+
+# Script generated for node Customer Trusted
+EvaluateDataQuality().process_rows(frame=PrivacyFilter_node1785108086162, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1785107557444", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+CustomerTrusted_node1785108199028 = glueContext.getSink(path="s3://stedi-lakehouse-2026/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1785108199028")
+CustomerTrusted_node1785108199028.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_trusted")
+CustomerTrusted_node1785108199028.setFormat("glueparquet", compression="uncompressed")
+CustomerTrusted_node1785108199028.writeFrame(PrivacyFilter_node1785108086162)
+job.commit()
